5e1d6b5923aef53c3ce0d97e26e98106a61d5805
Coleman Kane <ckane@colemankane.org>
2025-02-22 19:40:22 -0500

    Listen on all IPs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ nginx -p /app/exposure-front -c nginx.conf
 
 # Then, start up gunicorn which will remain in the foreground
 cd /app/exposure-back || (echo "Failed to change into /app/exposure-back directory"; exit 1)
-gunicorn -b '127.0.0.1:8001' -w 4 'exposure:app'
+gunicorn -b '0.0.0.0:8001' -w 4 'exposure:app'
